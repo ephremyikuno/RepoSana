3dc5048b1857fceb9165ed3b758131d10766fb57
Sana <khalil.sana@yahoo.com>
2018-05-11 21:29:47 -0400

    New Class Added

---
+++ Registration.java
@@ -0,0 +1,13 @@
+
+public class Registration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		driver WebDriver = new ChromeDriver ();
+		// This is GitHub example
+		
+		
+	}
+
+}
